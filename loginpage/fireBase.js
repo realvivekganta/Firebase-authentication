5f1c134ac214cd62099b364ae356539e65c89c94
Vivek Ganta <47542732+realvivekganta@users.noreply.github.com>
2019-06-28 11:58:52 -0400

    Add files via upload

---
+++ loginpage/fireBase.js
@@ -0,0 +1,15 @@
+var fireBase = {};
+(function(){
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDw4VMqaKF7ieOMmf_LiyOK7vkV3S8hwnQ",
+    authDomain: "budgetbanker-9f83e.firebaseapp.com",
+    databaseURL: "https://budgetbanker-9f83e.firebaseio.com",
+    projectId: "budgetbanker-9f83e",
+    storageBucket: "budgetbanker-9f83e.appspot.com",
+    messagingSenderId: "494875442768",
+    appId: "1:494875442768:web:2ecee4c308c4ae5e"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+})(); 
